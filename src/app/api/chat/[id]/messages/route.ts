6adc034592e306913d4beb8b198b1907149120af
JosephBFlannery <brooks.flannery2000@gmail.com>
2025-06-10 09:25:14 -0400

    bug4

--- src/app/api/chat/[id]/messages/route.ts
+++ src/app/api/chat/[id]/messages/route.ts
@@ -53,8 +53,13 @@ export async function POST(req: Request, context: { params: { id: string } }) {
   return result.toDataStreamResponse();
 }
 
-export async function GET( req : Request, context: { params: { id: string } }) {
-  const params = await context.params;
+type PostMessagesProp = {
+  params:{
+    id:string;
+  }
+}
+
+export async function GET( { params }: PostMessagesProp) {
   const chatId = Number(params.id);
   
   if (isNaN(chatId)) {
